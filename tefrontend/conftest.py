700b102f8750a4acab65c8d3be90cb9f1fb37618
TElphee01 <telphee2@gmail.com>
2022-02-26 11:33:30 -0500

    first commit

---
+++ tefrontend/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from tefrontend.users.models import User
+from tefrontend.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
